fc736962abaeda7f313455577f15beea5f3a8f81
roman431 <romanjacob32@gmail.com>
2022-12-08 17:10:45 -0800

    test

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,11 +13,11 @@ long int c;
 long long int d;
 float e;
 
-printf("The size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
-printf("The size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
-printf("The size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-printf("The size  of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
-printf("The size of a float: %lu byte(s)\n", (unsigned long)sizeof(e));
+printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(e));
 return (0);
 
 }
